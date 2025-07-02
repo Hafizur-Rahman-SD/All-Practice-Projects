d46eb8f2ac9f7743450755d96a051ec9745dbcaa
Hafizur Rahman <hrrony859@gmail.com>
2025-07-02 23:33:48 +0600

    Update JobItem.jsx

--- job-tracker/src/components/JobItem.jsx
+++ job-tracker/src/components/JobItem.jsx
@@ -5,10 +5,10 @@ import React from "react";
 function JobItem({ job, onDelete }) {
   return (
     <div className="job-item">
-      <h3>{job.companyName}</h3>              {/* Company name দেখাবে */}
+      <h3>{job.companyName}</h3>              {/* Show the Company name  */}
       <p>Position: {job.position}</p>        {/* Position */}
       <p>Status: {job.status}</p>            {/* Status */}
-      <p>Applied On: {job.dateTime}</p>      {/* নতুন date & time */}
+      <p>Applied On: {job.dateTime}</p>      {/* Show new date & time */}
       <button onClick={() => onDelete(job.id)}>Delete</button>
     </div>
   );
