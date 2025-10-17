d39b647c12b82d804628aae91750c2aed565cd3b
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-10-17 11:18:25 +0600

    Update JobItem.jsx

--- job-tracker/src/components/JobItem.jsx
+++ job-tracker/src/components/JobItem.jsx
@@ -6,7 +6,7 @@ function JobItem({ job, onDelete }) {
   return (
     <div className="job-item">
       <h3>{job.companyName}</h3> 
-      <p>C Name </p>             {/* Show the Company name  */}
+                  {/* Show the Company name  */}
       <p>Position: {job.position}</p>        {/* Position */}
       <p>Status: {job.status}</p>            {/* Status */}
       <p>Applied On: {job.dateTime}</p>      {/* Show new date & time */}
